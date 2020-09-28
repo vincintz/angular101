b3ada2165f3f1e500d9b4e36d9b8f836ed32846b
Theo Reyes <theo.reyes@simplyrfid.com>
2020-09-29 01:20:23 +0800

    Fixed unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular101'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular101');
+    expect(app.name).toEqual('world');
   });
 
   it('should render title', () => {
